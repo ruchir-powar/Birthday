332ed796daac4292e59d055d52dba7220e74ce6b
ruchir-powar <ruchir.powar@aurajewels.co.in>
2025-08-05 14:00:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.body.addEventListener('click', playMusic, { once: true });
 // ───── Countdown Timer ─────
 const timer = document.getElementById('timer');
 const second = 1000, minute = second * 60, hour = minute * 60, day = hour * 24;
-let countDown = new Date('Oct 22, 2023 00:00:00').getTime();
+let countDown = new Date('Aug 6, 2025 00:00:00').getTime();
 
 const x = setInterval(() => {
   const now = new Date().getTime();
@@ -516,3 +516,4 @@ function _slideThankYou() {
     }, 3000);
   }, 6000);
 }
+
